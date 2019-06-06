b28ae168a7d5f88d5f69bcf80c561d25a07ebb15
amitabhs30 <47911619+amitabhs30@users.noreply.github.com>
2019-06-06 21:36:01 +0530

    Main Shopping App

---
+++ app/src/main/java/com/example/android/ecommerce/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.ecommerce;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final ImageView logo=(ImageView)findViewById(R.id.logo);
+        super.onCreate(savedInstanceState);
+        final Intent Signup=new Intent(MainActivity.this,SignUp.class);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent screen= new Intent(MainActivity.this,FirstPage.class);
+                //startActivity(screen);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
