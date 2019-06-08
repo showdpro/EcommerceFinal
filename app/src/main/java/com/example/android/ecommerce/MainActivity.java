5ef571f9c6ad16d64c20b0b83139b18af9e2defb
amitabhs30 <47911619+amitabhs30@users.noreply.github.com>
2019-06-08 16:19:00 +0530

    linking UI
    Linking the Main activity with signUp and SignIn page and signUp and SignIn page with SignUp page

--- app/src/main/java/com/example/android/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/android/ecommerce/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent screen= new Intent(MainActivity.this,SignUp.class);
+                Intent screen= new Intent(MainActivity.this,Sign_up_and_Sign_in.class);
                 startActivity(screen);
                 finish();
             }
