22fea3135afa5698d7b7cbc00a0d615a869413d8
Sangwan100 <51412045+Sangwan100@users.noreply.github.com>
2019-06-09 21:18:25 +0530

    intent in setting activity for close button

--- app/src/main/java/com/example/android/ecommerce/SettingActivity.java
+++ app/src/main/java/com/example/android/ecommerce/SettingActivity.java
@@ -1,13 +1,25 @@
 package com.example.android.ecommerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
-
+    TextView close_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        close_button= findViewById(R.id.close_settings);
+
+        close_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent close = new Intent(SettingActivity.this,FirstPage.class);
+                startActivity(close);
+            }
+        });
     }
 }
