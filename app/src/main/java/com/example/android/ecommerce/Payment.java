81b86292049488d8743d06062b02b2f31c05487d
UtkarshGupta1999 <utkarsh1999gupta@gmail.com>
2019-06-21 15:34:09 +0530

    error fixed

--- app/src/main/java/com/example/android/ecommerce/Payment.java
+++ app/src/main/java/com/example/android/ecommerce/Payment.java
@@ -37,7 +37,7 @@ public class Payment extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mycart);
 
         mPaymentsClient =
                 Wallet.getPaymentsClient(
