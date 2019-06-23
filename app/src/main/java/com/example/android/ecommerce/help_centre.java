7caf83a09cb5fb8af57b2861167a8d2a0f9d5e3f
gvgh <sanchirastogi02@gmail.com>
2019-06-23 16:40:48 +0530

    help center added

---
+++ app/src/main/java/com/example/android/ecommerce/help_centre.java
@@ -0,0 +1,18 @@
+package com.example.android.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class help_centre extends AppCompatActivity {
+
+    ImageView call, mail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_centre);
+
+    }
+}
