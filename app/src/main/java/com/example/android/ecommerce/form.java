3ba5fc9b6474a6083a94be75db1a670dfd8f659e
Sangwan100 <51412045+Sangwan100@users.noreply.github.com>
2019-06-23 13:15:19 +0530

    layout-large is created for large screen size phone

--- app/src/main/java/com/example/android/ecommerce/form.java
+++ app/src/main/java/com/example/android/ecommerce/form.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
