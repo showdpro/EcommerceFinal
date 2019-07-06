8d167fd4934a5b9b92ad06be0ee808c3ba9df35e
gvgh <sanchirastogi02@gmail.com>
2019-07-06 13:52:10 +0530

    Revert "logo added"
    This reverts commit 3b8d5ac9736ae381fd12bf672f3873fc5346ab37.

--- app/src/main/java/com/example/android/ecommerce/ProjectDescription.java
+++ app/src/main/java/com/example/android/ecommerce/ProjectDescription.java
@@ -71,7 +71,10 @@ public class ProjectDescription extends AppCompatActivity {
       //  iconCart = findViewById(R.id.icon_cart);
 
 
+        //ViewPager viewpager = findViewById(R.id.temp_view_pager);
 
+        //ImageAdapter adapter = new ImageAdapter(this);
+        //viewpager.setAdapter(adapter);
         save = (Button) findViewById(R.id.Save_Button);
 
         editText1 = findViewById(R.id.temp_user_name);
